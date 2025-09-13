e35ec7ca335f767054f06b07cea8a350040a6c46
Juanita Jimenez <juanitatolosa94@gmail.com>
2025-09-13 00:57:02 -0500

    hice cambios en javascript y corregi errores, finalizado

--- js/main.js
+++ js/main.js
@@ -1,77 +1,88 @@
+
+    const contpre = document.getElementById('pre-simulation')
+    const contPost = document.getElementById('post-simulation')
+
+
+
 function simulador(){
-    const contPre = document.getElementById("pre-simulation");
-    const contPost = document.getElementById("post-simulation");
+    
 
-    // variables que capturan los valores de los campos
+    // VALORES DE LOS CAMPOS DEL FORMULARIO
+    let cNombres = document.getElementById('Nombres').value
+    let cTelefono = document.getElementById('Telefono').value
+    let cEmail = document.getElementById('Email').value
+    let cMonto = document.getElementById('Monto').value
+    let cTiempo = document.getElementById('Tiempo').value
 
-    let cNombres = document.getElementById("Nombres").value;
-    let cTelefono = document.getElementById("Telefono").value;
-    let cEmail = document.getElemetById("Email").value;
-    let cMonto = document.getElementById("Monto").value;
-    let cTiempo = document.getElementById("Tiempo").value;
+    // CONSTANTES QUE CAPTURAN LAS ETIQUETAS SPAN EN LAS QUE SE MUESTRAN LOS VALORES 
 
 
-// las etiquetas span en las que se muestran los valores por eso las s de show
-    const sTiempo = document.getElementById("sTiempo");
-    const sInteres = document.getElementById("sInteres");
-    const sTotal = document.getElementById("sTotal");
-    const sGanancia = document.getElementById("sGanancia");
-    const sNombres = document.getElementById("sNombres");
-    const sCorreo = document.getElementById("sCorreo");
 
-    // variables para realizar los campos
-    let ganancia = 0;
-    let total = 0;
+    const sTiempo = document.getElementById('sTiempo')
+    const sInteres = document.getElementById('sInteres')
+    const sTotal= document.getElementById('sTotal')
+    const sGanancia = document.getElementById('sGanancia')
+    const sNombres = document.getElementById('sNombres')
+    const sCorreo = document.getElementById('sCorreo')
 
-    if(cNombres=="" || cEmail=="" || cTelefono==""|| cMonto=="" || cTiempo==""){
+    // variables para realizar las operaciones 
+
+    let ganancia = 0
+    let total = 0
+
+
+    switch (cTiempo) {
+        // en este case realizaremos el calculo si el tiempo corresponde a 1 años
+        case '1':
+            ganancia = (cMonto*0.008)*12
+            total = parseInt (cMonto) + parseInt(ganancia)
+            sInteres.textContent = '0.8%'
+            sTiempo.textContent = '12 MESES'
+            break;
+
+        // en este case realizaremos el calculo si el tiempo corresponde a 2 años
+        case '2':
+            ganancia = (cMonto*0.013)*24
+            total = parseInt(cMonto) + parseInt(ganancia)
+            sInteres.textContent = '0.13%'
+            sTiempo.textContent = '24 MESES'
+            break;
+
+        // en este case realizaremos el calculo si el tiempo corresponde a 3 años
+        case '3':
+            ganancia = (cMonto * 0.017)*36
+            total = parseInt(cMonto) + parseInt(ganancia)
+            sInteres.textContent = '1.17%'
+            sTiempo.textContent = '36 MESES'
+        break;
+    
+        default:
+            break;
+    }
+
+    if (cNombres == "" || cEmail == "" || cMonto == "" || cTiempo == "") {
         alert("Complete los cambios obligatorios")
+        
     }else{
-    contPost.classList.remove('disabled'); 
-    contPre.classList.add('disabled');
-    
+        contPost.classList.remove('disabled')
+        contpre.classList.add('disabled')
         switch (cTiempo) {
-             case '1':
-            // en este case realizaremos el calculo si el tiempo corresponde a un año
-               ganancia = (cMonto * 0.008)*12;
-               total = parseInt(cMonto)  + parseInt(ganancia);
-               sInteres.textContent = '0.8';
-               sTiempo.textContent = '12 MESES';
-            break;
-             case '2':
-                // en este case realizremos el calculo si el tiempo corresponde a dos años
-                ganancia = (cMonto * 0.013)*24;
-                total = parseInt(cMonto)  + parseInt(ganancia);
-                sInteres.textContent = '1.3';
-                sTiempo.textContent = '24 MESES';
-            
-            break;
 
-             case '3':
-                // en este case realizremos el calculo si el tiempo corresponde a tres años
-                ganancia = (cMonto * 0.017)*36;
-                total = parseInt(cMonto)  + parseInt(ganancia);
-                sInteres.textContent = '1.7';
-                sTiempo.textContent = '36 MESES';
-            
-            break;
-    
+        }
     }
 
-    // aca estamos haciendo referencia a las etiquetas span para mostrar los valores
-    sTotal.textContent = total;
-    sGanancia.textContnt = ganancia;
-    sNombres.textContent = cNombres;
-    sCorreo.textContent = cEmail;
+    sTotal.textContent = total
+    sGanancia.textContent = ganancia
+    sNombres.textContent = cNombres
+    sCorreo.textContent = cEmail
 
 
+    contPost.classList.remove('disabled');
+    contpre.classList.add('disabled');
 
+
+    
 }
-
-
-document.addEventListener('DOMContentLoaded',function(){
-    document.getElementById('calcular').addEventListener('click', simulador);
-});
-
-    }
-
-    
+    document.addEventListener('DOMContentLoaded',function(){
+        document.getElementById('calcular').addEventListener('click',simulador);
+})
